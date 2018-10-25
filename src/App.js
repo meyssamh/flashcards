57aeb691c9c43b9b33f50b1bf7fae1ab428dc84d
Meyssam <meyssamh@web.de>
2018-10-25 15:36:45 +0330

    Setting up the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import classes from './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className={classes.App}>
-        <header className={classes.App_header}>
-          <img src={logo} className={classes.App_logo} alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className={classes.App_link}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        
       </div>
     );
   }
